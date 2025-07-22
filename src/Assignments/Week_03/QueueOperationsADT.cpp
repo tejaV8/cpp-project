28e172c357e0d71626516c91d23b1c8e7d79b3ee
Bhanu teja <bhanutejavittanala02@gmail.com>
2025-07-22 22:32:40 +0530

    Queue Operations ADT

---
+++ src/Assignments/Week_03/QueueOperationsADT.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+using namespace std;
+#define MAX 100
+int front = -1, rear = -1;
+int queue[MAX];
+
+void enqueue(int value) {
+    if (rear == MAX - 1) {
+        cout << "Queue Overflow\n";
+    }
+
+    cout << "Enqueuing " << value << " into queue.\n";
+    front = 0;
+    rear++;
+    queue[rear] = value;
+}
+
+void dequeue() {
+    if(front ==-1 && rear == -1) {
+        cout << "Queue Underflow\n";
+        return;
+    }
+    else if(front == rear) {
+        cout << "Dequeued " << queue[front] << " from queue.\n";
+        front = -1;
+        rear = -1;
+    } else {
+        cout << "Dequeued " << queue[front] << " from queue.\n";
+        front++;
+    }
+}
+
+void traversal() {
+    if (front == -1 && rear == -1) {
+        cout << "Queue is empty\n";
+        return;
+    }
+    cout << "Queue elements are: ";
+    for (int i = front; i <= rear; i++) {
+        cout << queue[i] << " ";
+    }
+    cout << endl;
+}
+
+void search(int value) {
+    if (front == -1 && rear == -1) {
+        cout << "Queue is empty\n";
+        return;
+    }
+
+    for (int i = front; i <= rear; i++) {
+        if (queue[i] == value) {
+            cout << "Element " << value << " found at index " << i << endl;
+            return;
+        }
+    }
+    cout << "Element " << value << " not found in queue\n";
+}
+
+int main() {
+    int choice, value;
+    cout << "---Queue Operations ADT---\n";
+    cout << "1. Enqueue\n";
+    cout << "2. Dequeue\n";
+    cout << "3. Traversal\n";
+    cout << "4. Search\n";
+    cout << "5. Exit\n";
+    do {
+        cout << "\nEnter your choice: ";
+        cin >> choice;
+
+        switch (choice) {
+            case 1:
+                cout << "Enter value to enqueue: ";
+                cin >> value;
+                enqueue(value);
+                break;
+            case 2:
+                dequeue();
+                break;
+            case 3:
+                traversal();
+                break;
+            case 4:
+                cout << "Enter value to search: ";
+                cin >> value;
+                search(value);
+                break;
+            case 5:
+                cout << "Exiting...\n";
+                break;
+            default:
+                cout << "Invalid choice! Please try again.\n";
+        }
+    } while (choice != 5);
+     
+    return 0;
+}
