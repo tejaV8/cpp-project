bad48d0a1f472fb624256b13600de02835c81388
Bhanu teja <bhanutejavittanala02@gmail.com>
2025-07-25 21:22:57 +0530

    Bubble Sort

--- src/Assignments/Week_02/BubbleSort.cpp
+++ src/Assignments/Week_02/BubbleSort.cpp
@@ -1,35 +1,50 @@
 #include <iostream>
 using namespace std;
 
-void bubbleSort(int arr[], int n) {
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
+void bubbleSort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        bool swapped = false;
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
                 swap(arr[j], arr[j + 1]);
+                swapped = true;
             }
         }
-        cout<<"After pass " << i + 1 << ": ";
-        for (int k = 0; k < n; k++) {
+        if (!swapped)
+        {
+            cout << "Array is already sorted.";
+            break;
+        }
+        cout << "After pass " << i + 1 << ": ";
+        for (int k = 0; k < n; k++)
+        {
             cout << arr[k] << " ";
         }
         cout << endl;
     }
 }
 
-int main() {
-     int size;
+int main()
+{
+    int size;
     cout << "Enter number of elements: ";
     cin >> size;
 
     int arr[size];
-    
+
     cout << "Enter elements: ";
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < size; i++)
+    {
         cin >> arr[i];
     }
     bubbleSort(arr, size);
     cout << "\nSorted array: ";
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < size; i++)
+    {
         cout << arr[i] << " ";
     }
     cout << endl;
