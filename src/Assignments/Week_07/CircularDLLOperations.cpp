1580a9cdd9fec34cfe65ffd5ad5e87bcc812984e
Bhanu teja <bhanutejavittanala02@gmail.com>
2025-07-31 00:08:59 +0530

    Doubly Linked Lists Operations

--- src/Assignments/Week_07/CircularDLLOperations.cpp
+++ src/Assignments/Week_07/CircularDLLOperations.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 using namespace std;
 
-struct node {
+struct node
+{
     int data;
     struct node *next;
     struct node *prev;
@@ -9,7 +10,8 @@ struct node {
 
 struct node *head = NULL, *tail = NULL, *New_node, *temp, *cur;
 
-void create() {
+void create()
+{
     New_node = new node();
     cout << "Enter data: ";
     cin >> New_node->data;
@@ -17,13 +19,17 @@ void create() {
     New_node->prev = NULL;
 }
 
-void insertAtBeginning() {
+void insertAtBeginning()
+{
     create();
-    if (head == NULL) {
+    if (head == NULL)
+    {
         head = tail = New_node;
 
         head->next = head->prev = head;
-    } else {
+    }
+    else
+    {
         New_node->next = head;
         head->prev = New_node;
         tail->next = New_node;
@@ -33,13 +39,17 @@ void insertAtBeginning() {
     cout << "Element " << New_node->data << " inserted at the beginning.\n";
 }
 
-void insertAtEnd() {
+void insertAtEnd()
+{
     create();
-    if (head == NULL) {
+    if (head == NULL)
+    {
         head = tail = New_node;
 
         head->next = head->prev = head;
-    } else {
+    }
+    else
+    {
         tail->next = New_node;
         New_node->prev = tail;
         New_node->next = head;
@@ -49,51 +59,66 @@ void insertAtEnd() {
     cout << "Element " << New_node->data << " inserted at the end.\n";
 }
 
-void insertAtPosition(int pos) {
-    if (pos < 1) {
+void insertAtPosition(int pos)
+{
+    if (pos < 1)
+    {
         cout << "Invalid position.\n";
         return;
     }
 
-    if (pos == 1) {
-        insertAtBeginning();
+    if (head == NULL)
+    {
+        create();
+        head = New_node;
+        New_node->next = head;
+        cout << "Element " << New_node->data << " inserted at position 1.\n";
         return;
     }
-
-    cur = head;
-    for (int i = 1; i < pos - 1; i++) {
-        if (cur->next == head) {
-            cout << "Position " << pos << " is out of bounds. Inserting at end.\n";
-            insertAtEnd();
-            return;
+    else
+    {
+        cur = head;
+        for (int i = 1; i < pos ; i++)
+        {
+            
+            if (cur->next == head)
+            {
+                cout << "Position " << pos << " is out of bounds. Inserting at end.\n";
+                insertAtEnd();
+                return;
+            }
+            cur = cur->next;
         }
-        cur = cur->next;
+        if(cur == head){
+            insertAtBeginning();
+        }
+        else{
+           create();
+           New_node->next = cur;
+           New_node->prev = cur->prev;
+           cur->prev->next = New_node;
+           cur->prev = New_node;
+        }
+        cout << "Element " << New_node->data << " inserted at position " << pos << ".\n";
     }
-
-    create();
-    New_node->next = cur->next;
-    New_node->prev = cur;
-
-    if (cur->next != head) {
-        cur->next->prev = New_node;
-    } else {
-        tail = New_node;
-    }
-
-    cur->next = New_node;
-    cout << "Element " << New_node->data << " inserted at position " << pos << ".\n";
 }
 
-void deleteFromBeginning() {
-    if (head == NULL) {
+void deleteFromBeginning()
+{
+    if (head == NULL)
+    {
         cout << "List is empty.\n";
         return;
-    } else if (head->next == head) {
+    }
+    else if (head->next == head)
+    {
         cout << "Deleted element: " << head->data << ".\n";
         delete head;
         head = tail = NULL;
         return;
-    } else {
+    }
+    else
+    {
         temp = head;
         head = head->next;
         head->prev = tail;
@@ -103,16 +128,22 @@ void deleteFromBeginning() {
     }
 }
 
-void deleteFromEnd() {
-    if (head == NULL) {
+void deleteFromEnd()
+{
+    if (head == NULL)
+    {
         cout << "List is empty.\n";
         return;
-    } else if (head->next == head) {
+    }
+    else if (head->next == head)
+    {
         cout << "Deleted element: " << head->data << ".\n";
         delete head;
         head = tail = NULL;
         return;
-    } else {
+    }
+    else
+    {
         temp = tail;
         tail = tail->prev;
         tail->next = head;
@@ -134,64 +165,78 @@ void deletionFromPosition(int pos) {
     }
 
     cur = head;
-    for (int i = 1; i < pos; i++) {
+    for (int i = 1; i < pos - 1; i++) {
+        cur = cur->next;
         if (cur->next == head) {
             cout << "Position " << pos << " does not exist.\n";
             return;
         }
-        cur = cur->next;
     }
 
-    if (cur == tail) {
+    temp = cur->next;
+
+    if (temp->next == head) {
         deleteFromEnd();
         return;
     }
 
-    cur->prev->next = cur->next;
-    cur->next->prev = cur->prev;
-    cout << "Deleted element: " << cur->data << ".\n";
-    delete cur;
+    cur->next = temp->next;
+    temp->next->prev = cur;
+
+    cout << "Deleted element: " << temp->data << ".\n";
+    delete temp;
 }
 
-void displayForward() {
-    if (head == NULL) {
+
+void displayForward()
+{
+    if (head == NULL)
+    {
         cout << "List is empty.\n";
         return;
     }
 
     temp = head;
     cout << "List elements (forward): ";
-    do {
+    do
+    {
         cout << temp->data << " ⇄ ";
         temp = temp->next;
     } while (temp != head);
     cout << "(head)\n";
 }
 
-void displayBackward() {
-    if (head == NULL) {
+void displayBackward()
+{
+    if (head == NULL)
+    {
         cout << "List is empty.\n";
         return;
     }
 
     temp = tail;
     cout << "List elements (backward): ";
-    do {
+    do
+    {
         cout << temp->data << " ⇄ ";
         temp = temp->prev;
     } while (temp != tail);
     cout << "(tail)\n";
 }
 
-void search(int ele) {
-    if (head == NULL) {
+void search(int ele)
+{
+    if (head == NULL)
+    {
         cout << "List is empty.\n";
         return;
     }
 
     temp = head;
-    do {
-        if (temp->data == ele) {
+    do
+    {
+        if (temp->data == ele)
+        {
             cout << "Element " << ele << " found in the list.\n";
             return;
         }
@@ -201,11 +246,12 @@ void search(int ele) {
     cout << "Element " << ele << " not found in the list.\n";
 }
 
-
-int main() {
+int main()
+{
 
     int choice, pos, ele;
-    do {
+    do
+    {
         cout << "\n--- Circular Doubly Linked List Menu ---\n";
         cout << "1. Insert at Beginning\n";
         cout << "2. Insert at End\n";
@@ -221,47 +267,48 @@ int main() {
         cout << "\nEnter your choice: ";
         cin >> choice;
 
-        switch (choice) {
-            case 1:
-                insertAtBeginning();
-                break;
-            case 2:
-                insertAtEnd();
-                break;
-            case 3:
-                cout << "Enter position: ";
-                cin >> pos;
-                insertAtPosition(pos);
-                break;
-            case 4:
-                deleteFromBeginning();
-                break;
-            case 5:
-                deleteFromEnd();
-                break;
-            case 6:
-                cout << "Enter position: ";
-                cin >> pos;
-                deletionFromPosition(pos);
-                break;
-            case 7:
-                displayForward();
-                break;
-            case 8:
-                displayBackward();
-                break;
-            case 9:
-                cout << "Enter element to search: ";
-                cin >> ele;
-                search(ele);
-                break;
-            case 10:
-                cout << "Exiting...\n";
-                break;
-            default:
-                cout << "Invalid choice. Try again.\n";
+        switch (choice)
+        {
+        case 1:
+            insertAtBeginning();
+            break;
+        case 2:
+            insertAtEnd();
+            break;
+        case 3:
+            cout << "Enter position: ";
+            cin >> pos;
+            insertAtPosition(pos);
+            break;
+        case 4:
+            deleteFromBeginning();
+            break;
+        case 5:
+            deleteFromEnd();
+            break;
+        case 6:
+            cout << "Enter position: ";
+            cin >> pos;
+            deletionFromPosition(pos);
+            break;
+        case 7:
+            displayForward();
+            break;
+        case 8:
+            displayBackward();
+            break;
+        case 9:
+            cout << "Enter element to search: ";
+            cin >> ele;
+            search(ele);
+            break;
+        case 10:
+            cout << "Exiting...\n";
+            break;
+        default:
+            cout << "Invalid choice. Try again.\n";
         }
     } while (choice != 10);
-     
+
     return 0;
 }
