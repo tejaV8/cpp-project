a42fec6c021087f0be0a0873d4a3ea97339928d7
Bhanu teja <bhanutejavittanala02@gmail.com>
2025-08-23 20:40:37 +0530

    Binary Search Tree

--- src/Assignments/Week_11/bst.cpp
+++ src/Assignments/Week_11/bst.cpp
@@ -1,132 +1,184 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
-struct node {
+struct node
+{
   int data;
-  struct node * left, * right;
+  struct node *left, *right;
 };
 
-struct node * root = NULL, * New_node, * cur, * parent;
+struct node *root = NULL, *New_node, *cur, *parent;
 
-void create() {
+void create()
+{
   New_node = new node();
   cout << "Enter data: ";
-  cin >> New_node -> data;
-  New_node -> left = NULL;
-  New_node -> right = NULL;
+  cin >> New_node->data;
+  New_node->left = NULL;
+  New_node->right = NULL;
 }
 
-void insert() {
-    create();
-    if (root == NULL) {
-        root = New_node;
-        cout << "Node " << New_node->data << " inserted." << endl;
-        return;
-    } else {
-        cur = root;
-        while (cur != NULL) {
-            if (New_node->data < cur->data) {
-                if (cur->left != NULL) {
-                    cur = cur->left;
-                } else {
-                    cur->left = New_node;
-                    cout << "Node " << New_node->data << " inserted." << endl;
-                    return;
-                }
-            } else if (New_node->data > cur->data) {
-                if (cur->right != NULL) {
-                    cur = cur->right;
-                } else {
-                    cur->right = New_node;
-                    cout << "Node " << New_node->data << " inserted." << endl;
-                    return;
-                }
-            } else {
-                // Duplicate node logic
-                cout << "Duplicate node " << New_node->data << " not inserted." << endl;
-                delete New_node;
-                return;
-            }
+void insert()
+{
+  create();
+  if (root == NULL)
+  {
+    root = New_node;
+    cout << "Node " << New_node->data << " inserted." << endl;
+    return;
+  }
+  else
+  {
+    cur = root;
+    while (cur != NULL)
+    {
+      if (New_node->data < cur->data)
+      {
+        if (cur->left != NULL)
+        {
+          cur = cur->left;
         }
+        else
+        {
+          cur->left = New_node;
+          cout << "Node " << New_node->data << " inserted." << endl;
+          return;
+        }
+      }
+      else if (New_node->data > cur->data)
+      {
+        if (cur->right != NULL)
+        {
+          cur = cur->right;
+        }
+        else
+        {
+          cur->right = New_node;
+          cout << "Node " << New_node->data << " inserted." << endl;
+          return;
+        }
+      }
+      else
+      {
+        // Duplicate node logic
+        cout << "Duplicate node " << New_node->data << " not inserted." << endl;
+        delete New_node;
+        return;
+      }
     }
+  }
 }
 
-void deleteNode(int ele) {
-  if (root == NULL) {
+void deleteNode(int ele)
+{
+  if (root == NULL)
+  {
     cout << "Tree is empty." << endl;
     return;
-  } else {
+  }
+  else
+  {
     cur = root;
     parent = NULL;
 
-    while (cur != NULL && cur -> data != ele) {
+    while (cur != NULL && cur->data != ele)
+    {
       parent = cur;
 
-      if (ele < cur -> data) {
-        cur = cur -> left;
-      } else {
-        cur = cur -> right;
+      if (ele < cur->data)
+      {
+        cur = cur->left;
+      }
+      else
+      {
+        cur = cur->right;
       }
     }
 
-    if (cur == NULL) {
+    if (cur == NULL)
+    {
       cout << "Element not found." << endl;
       return;
-    } else {
-      //Deletion of Leaf Node
-      if (cur -> left == NULL && cur -> right == NULL) {
-        if (cur == root) {
+    }
+    else
+    {
+      // Deletion of Leaf Node
+      if (cur->left == NULL && cur->right == NULL)
+      {
+        if (cur == root)
+        {
           root = NULL;
-        } else if (parent -> left == cur) {
-          parent -> left = NULL;
-        } else {
-          parent -> right = NULL;
+        }
+        else if (parent->left == cur)
+        {
+          parent->left = NULL;
+        }
+        else
+        {
+          parent->right = NULL;
         }
         cout << "Node " << ele << " deleted." << endl;
         delete cur;
       }
 
-      //Deletion of Node with one child - right
-      else if (cur -> left == NULL && cur -> right != NULL) {
-        if (cur == root) {
-          root = cur -> right;
-        } else if (parent -> left == cur) {
-          parent -> left = cur -> right;
-        } else {
-          parent -> right = cur -> right;
+      // Deletion of Node with one child - right
+      else if (cur->left == NULL && cur->right != NULL)
+      {
+        if (cur == root)
+        {
+          root = cur->right;
+        }
+        else if (parent->left == cur)
+        {
+          parent->left = cur->right;
+        }
+        else
+        {
+          parent->right = cur->right;
         }
         cout << "Node " << ele << " deleted." << endl;
         delete cur;
       }
 
-      //Deletion of Node with one child - left
-      else if (cur -> left != NULL && cur -> right == NULL) {
-        if (cur == root) {
-          root = cur -> left;
-        } else if (parent -> left == cur) {
-          parent -> left = cur -> left;
-        } else {
-          parent -> right = cur -> left;
+      // Deletion of Node with one child - left
+      else if (cur->left != NULL && cur->right == NULL)
+      {
+        if (cur == root)
+        {
+          root = cur->left;
+        }
+        else if (parent->left == cur)
+        {
+          parent->left = cur->left;
+        }
+        else
+        {
+          parent->right = cur->left;
         }
         cout << "Node " << ele << " deleted." << endl;
         delete cur;
       }
 
-      //Deletion of Node with 2 childs using Inorder Successor
-      else if (cur -> left && cur -> right) {
-        struct node * IS = cur -> right, * IP = NULL;
+      // Deletion of Node with 2 childs using Inorder Successor
+      else if (cur->left && cur->right)
+      {
+        struct node *IS = cur->right, *IP = NULL;
 
-        while (IS -> left != NULL) {
+        while (IS->left != NULL)
+        {
           IP = IS;
-          IS = IS -> left;
+          IS = IS->left;
         }
-        cur -> data = IS -> data;
+        cur->data = IS->data;
 
-        if (IP == NULL) {
-          cur -> right = IS -> right;
-        } else {
-          IP -> left = IS -> right;
+        if (IP == NULL)
+        {
+          cur->right = IS->right;
+        }
+        else
+        {
+          IP->left = IS->right;
         }
         cout << "Node " << ele << " deleted." << endl;
         delete IS;
@@ -135,41 +187,57 @@ void deleteNode(int ele) {
   }
 }
 
-void inOrderTraversal(struct node * cur) {
-  if (cur != NULL) {
-    inOrderTraversal(cur -> left);
-    cout << cur -> data << "->";
-    inOrderTraversal(cur -> right);
+void inOrderTraversal(struct node *cur)
+{
+  if (cur != NULL)
+  {
+    inOrderTraversal(cur->left);
+    cout << cur->data << "->";
+    inOrderTraversal(cur->right);
   }
 }
 
-void preOrderTraversal(struct node * cur) {
-  if (cur != NULL) {
-    cout << cur -> data << "->";
-    preOrderTraversal(cur -> left);
-    preOrderTraversal(cur -> right);
+void preOrderTraversal(struct node *cur)
+{
+  if (cur != NULL)
+  {
+    cout << cur->data << "->";
+    preOrderTraversal(cur->left);
+    preOrderTraversal(cur->right);
   }
 }
 
-void postOrderTraversal(struct node * cur) {
-  if (cur != NULL) {
-    postOrderTraversal(cur -> left);
-    postOrderTraversal(cur -> right);
-    cout << cur -> data << "->";
+void postOrderTraversal(struct node *cur)
+{
+  if (cur != NULL)
+  {
+    postOrderTraversal(cur->left);
+    postOrderTraversal(cur->right);
+    cout << cur->data << "->";
   }
 }
 
-void search(int ele) {
-  if (root == NULL) {
+void search(int ele)
+{
+  if (root == NULL)
+  {
     cout << "Tree is empty." << endl;
-  } else {
+  }
+  else
+  {
     cur = root;
-    while (cur != NULL) {
-      if (ele < cur -> data) {
-        cur = cur -> left;
-      } else if (ele > cur -> data) {
-        cur = cur -> right;
-      } else {
+    while (cur != NULL)
+    {
+      if (ele < cur->data)
+      {
+        cur = cur->left;
+      }
+      else if (ele > cur->data)
+      {
+        cur = cur->right;
+      }
+      else
+      {
         cout << "Element Found in tree." << endl;
         return;
       }
@@ -178,18 +246,21 @@ void search(int ele) {
   }
 }
 
-int main() {
+int main()
+{
   int choice, val;
 
   cout << "---Binary Search Tree Menu---" << endl;
 
   cout << "\n1. Insert\n2. Delete\n3. InOrder Traversal\n4. PreOrder Traversal\n5. PostOrder Traversal\n6. Search\n7. Exit\n";
 
-  while (true) {
+  while (true)
+  {
     cout << "\nEnter your choice: ";
     cin >> choice;
 
-    switch (choice) {
+    switch (choice)
+    {
     case 1:
       insert();
       break;
