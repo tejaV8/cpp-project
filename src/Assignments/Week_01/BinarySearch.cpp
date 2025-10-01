b06abcb592aeff833ceca5902ea80e54c93d48b9
Bhanu teja <bhanutejavittanala02@gmail.com>
2025-10-01 13:45:15 +0530

    Binary Search

--- src/Assignments/Week_01/BinarySearch.cpp
+++ src/Assignments/Week_01/BinarySearch.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 using namespace std;
 
-int binarySearchIterative(int arr[], int n, int key) {
+int binarySearchIterative(int arr[], int n, int key)
+{
     int low = 0, high = n - 1;
 
-    while (low <= high) {
-        int mid = (low + high) / 2;
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
 
         if (arr[mid] == key)
             return mid;
@@ -18,11 +20,12 @@ int binarySearchIterative(int arr[], int n, int key) {
     return -1;
 }
 
-int binarySearchRecursive(int arr[], int low, int high, int key) {
+int binarySearchRecursive(int arr[], int low, int high, int key)
+{
     if (low > high)
         return -1;
 
-    int mid = (low + high) / 2;
+    int mid = low + (high - low) / 2;
 
     if (arr[mid] == key)
         return mid;
@@ -32,18 +35,23 @@ int binarySearchRecursive(int arr[], int low, int high, int key) {
         return binarySearchRecursive(arr, low, mid - 1, key);
 }
 
-int main() {
-    int n, arr[100];
-    int choice, key, result;
+int main()
+{
+    int n, choice, key, result;
 
     cout << "Enter number of elements (sorted): ";
     cin >> n;
+
+    int *arr = new int[n];
+
     cout << "Enter " << n << " sorted elements: ";
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         cin >> arr[i];
     }
 
-    do {
+    do
+    {
         cout << "\n---- Binary Search Menu ----\n";
         cout << "1. Iterative Binary Search\n";
         cout << "2. Recursive Binary Search\n";
@@ -51,36 +59,38 @@ int main() {
         cout << "Enter your choice: ";
         cin >> choice;
 
-        switch(choice) {
-            case 1:
-                cout << "Enter element to search: ";
-                cin >> key;
-                result = binarySearchIterative(arr, n, key);
-                if (result != -1)
-                    cout << "Element found at index: " << result << endl;
-                else
-                    cout << "Element not found.\n";
-                break;
+        switch (choice)
+        {
+        case 1:
+            cout << "Enter element to search: ";
+            cin >> key;
+            result = binarySearchIterative(arr, n, key);
+            if (result != -1)
+                cout << "Element found at index: " << result << endl;
+            else
+                cout << "Element not found.\n";
+            break;
 
-            case 2:
-                cout << "Enter element to search: ";
-                cin >> key;
-                result = binarySearchRecursive(arr, 0, n - 1, key);
-                if (result != -1)
-                    cout << "Element found at index: " << result << endl;
-                else
-                    cout << "Element not found.\n";
-                break;
+        case 2:
+            cout << "Enter element to search: ";
+            cin >> key;
+            result = binarySearchRecursive(arr, 0, n - 1, key);
+            if (result != -1)
+                cout << "Element found at index: " << result << endl;
+            else
+                cout << "Element not found.\n";
+            break;
 
-            case 3:
-                cout << "Exiting...\n";
-                break;
+        case 3:
+            cout << "Exiting...\n";
+            break;
 
-            default:
-                cout << "Invalid choice. Try again.\n";
+        default:
+            cout << "Invalid choice. Try again.\n";
         }
 
     } while (choice != 3);
+
     cout << endl;
     return 0;
 }
