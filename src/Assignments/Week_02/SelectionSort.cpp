e790d9312660a9ba5e9cfdba0c61e0046f30f1ac
Bhanu teja <bhanutejavittanala02@gmail.com>
2025-10-01 16:11:11 +0530

    Selection Sort

--- src/Assignments/Week_02/SelectionSort.cpp
+++ src/Assignments/Week_02/SelectionSort.cpp
@@ -6,6 +6,8 @@ void selectionSort(int arr[], int n)
     for (int i = 0; i < n - 1; i++)
     {
         int minIndex = i;
+        bool swapped = false;
+
         for (int j = i + 1; j < n; j++)
         {
             if (arr[j] < arr[minIndex])
@@ -13,16 +15,25 @@ void selectionSort(int arr[], int n)
                 minIndex = j;
             }
         }
+
         if (i != minIndex)
         {
             swap(arr[i], arr[minIndex]);
+            swapped = true;
         }
+
         cout << "After pass " << i + 1 << ": ";
         for (int k = 0; k < n; k++)
         {
             cout << arr[k] << " ";
         }
         cout << endl;
+
+        if (!swapped)
+        {
+            cout << "No swaps in this pass → Array is already sorted." << endl;
+            break;
+        }
     }
 }
 
@@ -40,12 +51,16 @@ int main()
         cin >> arr[i];
     }
     cout << endl;
+
+    cout << "Implementing Selection Sort..." << endl;
     selectionSort(arr, size);
+
     cout << "\nSorted array: ";
     for (int i = 0; i < size; i++)
     {
         cout << arr[i] << " ";
     }
     cout << endl;
+
     return 0;
 }
