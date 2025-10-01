23cbfe87222aaa9ea9e5c88b376811117e7c7cee
Bhanu teja <bhanutejavittanala02@gmail.com>
2025-10-01 14:22:49 +0530

    Insertion Sort

--- src/Assignments/Week_02/InsertionSort.cpp
+++ src/Assignments/Week_02/InsertionSort.cpp
@@ -8,10 +8,13 @@ void insertionSort(int arr[], int n)
         int temp = arr[i];
         int j = i - 1;
 
+        bool shifted = false;
+
         while (j >= 0 && arr[j] > temp)
         {
             arr[j + 1] = arr[j];
             j--;
+            shifted = true;
         }
         arr[j + 1] = temp;
 
@@ -21,6 +24,12 @@ void insertionSort(int arr[], int n)
             cout << arr[k] << " ";
         }
         cout << endl;
+
+        if (!shifted)
+        {
+            cout << "No shifts in this pass → Array is already sorted." << endl;
+            break;
+        }
     }
 }
 
@@ -38,12 +47,16 @@ int main()
         cin >> arr[i];
     }
     cout << endl;
+
+    cout << "Implementing Insertion Sort..." << endl;
     insertionSort(arr, size);
+
     cout << "\nSorted array: ";
     for (int i = 0; i < size; i++)
     {
         cout << arr[i] << " ";
     }
     cout << endl;
+
     return 0;
 }
